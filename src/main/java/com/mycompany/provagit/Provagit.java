5cbb0c9b1eb08ef0c799cbc5063c3e4af40fc06e
toni fernandez <toni.fernandez@cirvianum.cat>
2022-10-18 12:14:07 +0200

    Hola Mon traduirt al català

--- src/main/java/com/mycompany/provagit/Provagit.java
+++ src/main/java/com/mycompany/provagit/Provagit.java
@@ -12,6 +12,6 @@ package com.mycompany.provagit;
 public class Provagit {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Hola Mon");
     }
 }
